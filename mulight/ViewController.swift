d0197f2f207b10b6c3c4dcbb2d1238dc70e17fab
Jukui <liujukui@gmail.com>
2019-07-28 01:31:47 +0800

    init the project

---
+++ mulight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mulight
+//
+//  Created by jukui liu on 2019/7/27.
+//  Copyright © 2019 jukui liu. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
